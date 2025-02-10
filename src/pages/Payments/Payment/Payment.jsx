606ad527c8cede3d94a1ed8fbd0b55f5c789bfd9
ElminAkberov <ekberovelmin04@gmail.com>
2025-02-10 21:26:32 +0400

    10.06

--- src/pages/Payments/Payment/Payment.jsx
+++ src/pages/Payments/Payment/Payment.jsx
@@ -6,7 +6,7 @@ const Payment = () => {
   return (
     <menu className="flex overflow-hidden items-center justify-center w-full">
       <div className="w-[100%]">
-        <div className="w-full lg:flex gap-x-3 items-center p-6">
+        {/* <div className="w-full lg:flex gap-x-3 items-center p-6">
           <input
             className="w-full rounded-lg border border-[#3d4d60] satoshi bg-white py-2 px-5 text-black outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0]  "
             placeholder="Поиск"
@@ -25,7 +25,7 @@ const Payment = () => {
               <p>Создать заявку</p>
             </button>
           </NavLink>
-        </div>
+        </div> */}
         <PaymentList />
       </div>
     </menu>
